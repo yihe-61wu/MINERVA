1ef506e6a4cae94e775ec4857f92256f15b758cd
L.Yihe <anish_alpha@hotmail.com>
2021-01-30 19:33:19 +0000

    add class Minerva2

--- minerva.py
+++ minerva.py
@@ -2,9 +2,23 @@
 """
 Created on Sat Jan 30 18:05:17 2021
 
-@author: anish
+@author: Lu, Yihe
 
-Original MINERVA
+Original Minverva2
 """
 
 import numpy as np
+
+class Minerva2:
+    def __init__(self, trace_size):
+        self.trace_size = trace_size
+        self.reset()
+    
+    def reset(self):
+        self.memory = np.empty((0, self.trace_size))
+
+    def learn(learning_data, learning_rate = 1):
+        _data = canonicalisation(learning_data, self.trace_size)
+        self.memory = np.concatenate((self.memory, _data), axis = 0)
+        
+    def respond():
