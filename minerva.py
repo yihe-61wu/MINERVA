0895f988e5292c8127c8081af5d0902a0454312b
L.Yihe <anish_alpha@hotmail.com>
2021-01-31 10:08:36 +0000

    refine Minerva2._probe_similarity()

--- minerva.py
+++ minerva.py
@@ -4,12 +4,18 @@ Created on Sat Jan 30 18:05:17 2021
 
 @author: Lu, Yihe
 
-Original Minverva2
+Original Minverva2 from Hintzman (1984)
 """
 
 import numpy as np
 
-class Minerva2:
+def proba0(x):
+    _x = np.asarray(x)
+    _x[_x > 1], _x[_x < -1] = 1, -1
+    p = 1 - np.abs(_x)
+    return p
+
+class Minerva2: # Hintzman (1984)
     def __init__(self, trace_size):
         self.trace_size = trace_size
         self.reset()
@@ -24,7 +30,11 @@ class Minerva2:
     def respond(self, probes, recurrence = 1):
         pass
     
-    def _probe_similarity(self, probe):
+    def _probe_similarity(self, probe): # Eq.1
         numer = np.dot(self.memory, probe)
-        denom = 0
-        return np.divide(numer, denom)
+        denom = self.trace_size - np.dot(proba0(self.memory), proba0(probe))
+        if denom == 0:
+            sim = np.zeros(len(probe))
+        else:
+            sim = np.divide(numer, denom)
+        return sim
