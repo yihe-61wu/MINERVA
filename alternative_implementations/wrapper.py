9e74fef7b11fbb546017242e602fa341b0a5c3f3
L.Yihe <anish_alpha@hotmail.com>
2021-02-04 15:35:41 +0000

    add model2: rpy2 working

--- alternative_implementations/wrapper.py
+++ alternative_implementations/wrapper.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 import minerva2 as dwhite54
+import rpy2.robjects as robjects
 
 class model1(dwhite54.Minerva2):
     def reset(self):
@@ -15,5 +16,25 @@ class model1(dwhite54.Minerva2):
             echo = self._echo(echo)[1]
         return echo
     
-    def _echo(self, probs):
+    def _echo(self, probes):
+        intensity, activation = self.get_echo_intensities(np.reshape(probs, (-1, self.features_per_trace)), 0)        
+        content = np.dot(activation, self.model)
+        normalised_echo = content / np.amax(np.abs(content), axis = 1).reshape((-1, 1))
+        return intensity, normalised_echo
+
+class model2:
+    def __init__(self, feature_num):
+        deniztu = robjects.r['source']
+        deniztu("minerva-al.R")
+        self.r_probe_memory = robjects.globalenv['probe_memory']
+        self.r_expect_event = robjects.globalenv['expect_event']
+        self.r_learn = robjects.globalenv['learn']
+        ###
+        self.feature_num = feature_num
+        self.reset()
+    
+    def reset(self):
+        self.memory = np.empty((0, self.trace_size))
         
+    def learn(self, learning_data):
+        pass
